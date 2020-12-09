1767cbc57292bf6202cb90271d98d0d6b3f8ef73
mehrabsha <44315888+mehrabsha@users.noreply.github.com>
2020-12-09 16:40:51 +0330

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <router-link
       v-for="post in edges"
-      :to="{ name: 'post', params: { slug: post.node.title } }"
+      :to="{ name: 'post', params: { id: post.node.id } }"
       :key="post.node.title"
     >
       <article>
